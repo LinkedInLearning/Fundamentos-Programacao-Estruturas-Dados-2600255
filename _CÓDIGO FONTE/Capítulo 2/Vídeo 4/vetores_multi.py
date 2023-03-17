f506534d1d70aae6b414b200fb36fef7c3af3dad
Lucas Longo <67756611+lucaslongolinkedin@users.noreply.github.com>
2023-03-17 17:59:54 +0000

    small changes

--- _CÓDIGO FONTE/Capítulo 2/Vídeo 4/vetores_multi.py
+++ _CÓDIGO FONTE/Capítulo 2/Vídeo 4/vetores_multi.py
@@ -1,25 +1,26 @@
-tarefas_segunda = ["Tarefa A", "Tarefa B", "Tarefa C"]
-tarefas_terca = ["Tarefa D", "Tarefa E", "Tarefa F"]
-tarefas_quarta = ["Tarefa G", "Tarefa H", "Tarefa I"]
+var tarefas_segunda = ["Tarefa A", "Tarefa B", "Tarefa C"];
+var tarefas_terca = ["Tarefa D", "Tarefa E", "Tarefa F"];
+var tarefas_quarta = ["Tarefa G", "Tarefa H", "Tarefa I"];
 
-dias_tarefas = [
-    tarefas_segunda,
-    tarefas_terca,
-    tarefas_quarta,
-]
+var dias_tarefas = [
+  tarefas_segunda,
+  tarefas_terca,
+  tarefas_quarta,
+];
 
 primeira_tarefa_terca = dias_tarefas[1][0]
 
-print("1) Primeira tarefa da terça: " + primeira_tarefa_terca)
+console.log("1) Primeira tarefa da terça: " + primeira_tarefa_terca)
 
 dias_tarefas = [
-    ["Tarefa A", "Tarefa B", "Tarefa C"],
-    ["Tarefa D", "Tarefa E", "Tarefa F"],
-    ["Tarefa G", "Tarefa H", "Tarefa I"],
+  ["Tarefa A", "Tarefa B", "Tarefa C"],
+  ["Tarefa D", "Tarefa E", "Tarefa F"],
+  ["Tarefa G", "Tarefa H", "Tarefa I"],
 ]
 
 tarefas_terca = dias_tarefas[1]
 primeira_tarefa_terca = tarefas_terca[0]
 
-print("2) Primeira tarefa da terça: " + primeira_tarefa_terca)
+console.log("2) Primeira tarefa da terça: " + primeira_tarefa_terca)
+
 
