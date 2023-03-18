4ade4ac085f9a9cd880f6865102d6975c08b3b1c
Lucas Longo <67756611+lucaslongolinkedin@users.noreply.github.com>
2023-03-18 15:05:53 +0000

    vetor mutável

--- _CÓDIGO FONTE/Capítulo 2/06/vetor_mutavel.py
+++ _CÓDIGO FONTE/Capítulo 2/06/vetor_mutavel.py
@@ -1,22 +1,40 @@
-
-def existe_fruta(nomeFruta):
-    existe = nomeFruta in frutas
-    mensagem = nomeFruta
-    if existe:
-        mensagem += " existe no vetor"
-    else: 
-        mensagem += " não existe no vetor"
-    print(mensagem)
-    
+print("\n1) Vetor vazio")
 frutas = []
 print(frutas)
 
+print("\n2) Append (adicionar ao final)")
 frutas.append("Maçã")
 print(frutas)
 
-outras_frutas = ["Banana", "Limão"]
+print("\n3) Somar um vetor com outro")
+outras_frutas = ["Banana", "Limão", "Laranja", "Uva", "Abacate", 2]
 frutas += outras_frutas
 print(frutas)
 
-existe_fruta("Limão")
-existe_fruta("xxx")
+print("\n4) Pop (remover último elemento)")
+frutas.pop()
+print(frutas)
+
+print("\n5) Pop índice 2 (remover item do índice 2)")
+frutas.pop(2)
+print(frutas)
+
+print("\n6) Remove (remover)")
+frutas.remove("Maçã")
+print(frutas)
+
+print("\n7) Append (colocar no final)")
+frutas.append("Maça")
+print(frutas)
+
+print("\n8) Inserir no índice 1")
+frutas.insert(1, "Pera")
+print(frutas)
+
+print("\n9) Inverter a ordem")
+frutas.reverse()
+print(frutas)
+
+print("\n10) Ordenar")
+frutas.sort()
+print(frutas)
